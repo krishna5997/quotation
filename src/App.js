7c5fbbfb8c2b34789b220b05193f212356ffce91
krishna5997 <swelinhtut7@gmail.com>
2022-01-11 11:31:59 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import QuotationTable from "./QuotationTable";
+
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+import useLocalStorage from 'react-localstorage-hook'
 
 function App() {
+  const itemRef = useRef();
+  const ppuRef = useRef();
+  const qtyRef = useRef();
+
+  // const [dataItems, setDataItems] = useState([]);
+  const [dataItems, setDataItems] = useLocalStorage("dataItems",[]);
+  
+
+
+  const dummyProductList = [
+    { id: "p001", name: 'Gigabyte Aorus 15P YD"', price: 65000 },
+    { id: "p002", name: "MSI GP66 Leopard", price: 60000 },
+    { id: "p003", name: "Razer Blade 15", price: 55000 },
+    { id: "p004", name: "AUSU ROG G14", price: 53000 },
+  ];
+
+  const addItem = () => {
+    if (itemRef.current.value == "") {
+      alert("Item name is empty");
+      return;
+    }
+
+    const pid = itemRef.current.value;
+    const product = dummyProductList.find((e) => e.id === pid);
+
+    var itemObj = {
+      pid: pid,
+      item: product.name,
+      ppu: ppuRef.current.value,
+      qty: qtyRef.current.value,
+    };
+
+    dataItems.push(itemObj);
+    setDataItems([...dataItems]);   
+  };
+
+  const productChange = (e) => {
+    const pid = itemRef.current.value;
+    const product = dummyProductList.find((e) => e.id === pid);
+    ppuRef.current.value = product.price
+  }
+
+  const options = dummyProductList.map((v) => {
+    return <option value={v.id}>{v.name}</option>;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={5} style={{ backgroundColor: "#eaeaea" }}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formItem">
+              <Form.Label>Item</Form.Label>
+              <Form.Select
+                aria-label="Default select example"
+                ref={itemRef}
+                onChange={productChange}
+              >
+                {options}
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Price Per Unit"
+                ref={ppuRef}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formQauntity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control type="number" placeholder="Quantity" ref={qtyRef} />
+            </Form.Group>
+
+            <Button variant="outline-dark" onClick={addItem}>
+              Add
+            </Button>
+          </Form>
+        </Col>
+        <Col>
+          <QuotationTable data={dataItems} setDataItems={setDataItems} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
